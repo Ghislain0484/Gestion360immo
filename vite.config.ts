b1369714a36c8c2cde8dad60bddfec4a56e227e1
Maurel84 <ton-email-li-au-compte-Maurel84>
2025-08-20 09:52:20 +0000

    chore(build): enable production sourcemaps for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ build: { sourcemap: true },
   plugins: [react()],
   build: {
     outDir: 'dist',
